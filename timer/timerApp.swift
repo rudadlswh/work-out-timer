9a7e0fd334c7371b6de5dfc209b2cc75765fc33a
whrudals <96468366+rudadlswh@users.noreply.github.com>
2026-02-05 14:33:49 +0900

    Initial Commit

---
+++ timer/timerApp.swift
@@ -0,0 +1,17 @@
+//
+//  timerApp.swift
+//  timer
+//
+//  Created by 조경민 on 2/5/26.
+//
+
+import SwiftUI
+
+@main
+struct timerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
